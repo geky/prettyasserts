f2e510c378e3ce0474bfc02152409013f8b06f56
Christopher Haster <geky@geky.net>
2024-02-01 18:09:38 -0600

    Attempted better Rc impl, not working
    It's always fun when Rust segfaults

--- src/rc.rs
+++ src/rc.rs
@@ -4,19 +4,18 @@ use std::marker::PhantomData;
 use std::alloc::Layout;
 use std::cell::Cell;
 use std::cmp::max;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 use std::mem::ManuallyDrop;
 use std::mem::size_of;
 use std::mem::align_of;
-use std::mem::size_of_val;
+use std::mem::transmute;
 
-// global collection of refcounts
-thread_local! {
-    static MAP: RefCell<HashMap<*const (), usize>>
-        = RefCell::new(HashMap::new());
+struct RcBoxHeader<T: ?Sized> {
+    rc: Cell<usize>,
+    // we need this to preserve metadata for zero-sized types
+    ptr: Option<NonNull<RcBox<T>>>,
 }
+
+struct RcBox<T: ?Sized>(RcBoxHeader<T>, T);
 
 // A transmutable Rc type
 #[repr(transparent)]
@@ -25,42 +24,64 @@ pub struct Rc<T: ?Sized> {
     phantom: PhantomData<T>,
 }
 
-impl<T: ?Sized> From<Box<T>> for Rc<T> {
-    fn from(box_: Box<T>) -> Self {
-        let ptr = NonNull::from(Box::leak(box_));
-        if size_of_val::<T>(unsafe { ptr.as_ref() }) > 0 {
-            MAP.with(|map| {
-                match map.borrow_mut().insert(ptr.as_ptr() as *const (), 1) {
-                    Some(_) => unreachable!(),
-                    None => {}
-                }
-            });
-        }
+impl<T: ?Sized> RcBoxHeader<T> {
+    fn new() -> Self {
+        RcBoxHeader{rc: Cell::new(1), ptr: None}
+    }
 
+    fn layout(t: &T) -> Layout {
+        Layout::new::<RcBoxHeader<T>>()
+            .extend(Layout::for_value(t))
+            .unwrap()
+            .0
+    }
+
+    fn offset(t: &T) -> usize {
+        Layout::new::<RcBoxHeader<T>>()
+            .extend(Layout::for_value(t))
+            .unwrap()
+            .1
+    }
+}
+
+impl<T: ?Sized> Rc<T> {
+    fn rcbox(&self) -> &RcBox<T> {
+        unsafe {
+            ((self.ptr.as_ptr() as *const u8).sub(
+                RcBoxHeader::offset(self.ptr.as_ref())
+            ) as *const RcBoxHeader<T>)
+                .as_ref().unwrap()
+                .ptr.unwrap().as_ref()
+        }
+    }
+
+    fn rc(&self) -> &Cell<usize> {
+        &self.rcbox().0.rc
+    }
+
+    fn from_rcbox(mut rcbox: Box<RcBox<T>>) -> Self {
+        rcbox.0.rc = Cell::new(1);
+        rcbox.0.ptr = Some(NonNull::from(rcbox.as_ref()));
+        let ptr = NonNull::from(&Box::leak(rcbox).1);
         Rc{ptr: ptr, phantom: PhantomData}
+    }
+
+    unsafe fn into_rcbox(&self) -> Box<RcBox<T>> {
+        Box::from_raw(self.rcbox() as *const RcBox<T> as *mut RcBox<T>)
     }
 }
 
 impl<T> Rc<T> {
     pub fn new(t: T) -> Self {
-        Self::from(Box::new(t))
+        let rcbox = Box::new(RcBox(RcBoxHeader::new(), t));
+        Self::from_rcbox(rcbox)
     }
 }
 
 impl<T: ?Sized> Clone for Rc<T> {
     fn clone(&self) -> Self {
-        if size_of_val::<T>(unsafe { self.ptr.as_ref() }) > 0 {
-            MAP.with(|map| {
-                match map.borrow_mut().entry(self.ptr.as_ptr() as *const ()) {
-                    Entry::Occupied(mut entry) => {
-                        let mut rc = *entry.get();
-                        rc += 1;
-                        *entry.get_mut() = rc;
-                    }
-                    Entry::Vacant(entry) => unreachable!(),
-                }
-            });
-        }
+        let rc = self.rc().get();
+        self.rc().set(rc + 1);
 
         Rc{ptr: self.ptr, phantom: PhantomData}
     }
@@ -68,57 +89,28 @@ impl<T: ?Sized> Clone for Rc<T> {
 
 impl<T: ?Sized> Drop for Rc<T> {
     fn drop(&mut self) {
-        let zero = if size_of_val::<T>(unsafe { self.ptr.as_ref() }) > 0 {
-            MAP.with(|map| {
-                match map.borrow_mut().entry(self.ptr.as_ptr() as *const ()) {
-                    Entry::Occupied(mut entry) => {
-                        let mut rc = *entry.get();
-                        rc -= 1;
-                        if rc == 0 {
-                            entry.remove();
-                            true
-                        } else {
-                            *entry.get_mut() = rc;
-                            false
-                        }
-                    }
-                    Entry::Vacant(entry) => unreachable!(),
-                }
-            })
+        let rc = self.rc().get();
+        if rc == 1 {
+            //drop(unsafe { self.into_rcbox() });
         } else {
-            true
-        };
-
-        if zero {
-            drop(unsafe { Box::from_raw(self.ptr.as_ptr()) });
+            self.rc().set(rc - 1);
         }
     }
 }
 
 impl<T: Clone> Rc<T> {
     pub fn try_unwrap(self) -> Result<T, Rc<T>> {
-        let zero = if size_of_val::<T>(unsafe { self.ptr.as_ref() }) > 0 {
-            MAP.with(|map| {
-                match map.borrow_mut().entry(self.ptr.as_ptr() as *const ()) {
-                    Entry::Occupied(entry) => {
-                        let mut rc = *entry.get();
-                        rc -= 1;
-                        if rc == 0 {
-                            entry.remove();
-                            true
-                        } else {
-                            false
-                        }
-                    }
-                    Entry::Vacant(entry) => unreachable!(),
-                }
-            })
-        } else {
-            true
-        };
-
-        if zero {
-            Ok(unsafe { std::ptr::read(ManuallyDrop::new(self).ptr.as_ptr()) })
+        let rc = self.rc().get();
+        if rc == 1 {
+            unsafe {
+                let mut rcbox = transmute::<
+                    Box<RcBox<T>>,
+                    Box<RcBox<ManuallyDrop<T>>>
+                >(
+                    self.into_rcbox()
+                );
+                Ok(ManuallyDrop::take(&mut rcbox.1))
+            }
         } else {
             Err(self)
         }
@@ -138,38 +130,13 @@ impl<T: Clone> Rc<T> {
 
 
 
-// to/from references
+// traits
 impl<T: ?Sized> AsRef<T> for Rc<T> {
     fn as_ref(&self) -> &T {
         unsafe { self.ptr.as_ref() }
     }
 }
 
-impl<T: ?Sized> Rc<T> {
-    pub fn try_from(t: &T) -> Option<Rc<T>> {
-        let found = MAP.with(|map| {
-            match map.borrow_mut().entry(t as *const T as *const ()) {
-                Entry::Occupied(mut entry) => {
-                    let mut rc = *entry.get();
-                    rc += 1;
-                    *entry.get_mut() = rc;
-                    true
-                }
-                Entry::Vacant(entry) => {
-                    false
-                }
-            }
-        });
-
-        if found {
-            Some(Rc{ptr: NonNull::from(t), phantom: PhantomData})
-        } else {
-            None
-        }
-    }
-}
-
-// other traits
 impl<T: ?Sized> std::ops::Deref for Rc<T> {
     type Target = T;
 
@@ -200,9 +167,37 @@ impl<T: ?Sized + Default> Default for Rc<T> {
 }
 
 // other casts
+impl<T> Rc<[T]> {
+    unsafe fn from_slice(src: &mut [ManuallyDrop<T>]) -> Rc<[T]> {
+        let raw = std::alloc::alloc(RcBoxHeader::layout(src));
+        let hdr = raw as *mut RcBoxHeader<[T]>;
+        hdr.write(RcBoxHeader::new());
+
+        let dst = raw.add(RcBoxHeader::offset(src)) as *mut T;
+        for i in 0..src.len() {
+            dst.write(ManuallyDrop::take(&mut src[i]));
+        }
+
+        let dst = std::slice::from_raw_parts_mut(dst, src.len());
+        Rc{ptr: NonNull::from(dst), phantom: PhantomData}
+    }
+}
+
+impl<T> From<Box<[T]>> for Rc<[T]> {
+    fn from(b: Box<[T]>) -> Self {
+        unsafe {
+            let mut b = transmute::<Box<[T]>, Box<[ManuallyDrop<T>]>>(b);
+            Self::from_slice(&mut b)
+        }
+    }
+}
+
 impl<T> From<Vec<T>> for Rc<[T]> {
     fn from(v: Vec<T>) -> Self {
-        Self::from(v.into_boxed_slice())
+        unsafe {
+            let mut v = transmute::<Vec<T>, Vec<ManuallyDrop<T>>>(v);
+            Self::from_slice(&mut v)
+        }
     }
 }
 
